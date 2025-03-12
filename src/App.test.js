45829e8b321ae75c82fc04415b5d7db72ce1477e
unknown <PhamVanLuc962005>
2025-03-12 15:15:17 +0700

    Bai Kiem Tra

---
+++ src/App.test.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+ import PvlMemberList from "./components/PvlMemberList";
+ import PvlMemberAdd from "./components/PvlMemberAdd";
+ 
+ 
+ 
+ const PvlApp = () => {
+   //khởi tạo thành viên
+   const [members, setMembers] = useState([
+     { id: "230001", fullname: "Nguyễn Văn Kiên", username: "Test1", password: "123456" },
+     { id: "230002", fullname: "Trần Thanh Hòa", username: "C", password: "abcdef" },
+     { id: "230003", fullname: "Lê Quang Quân", username: "B", password: "qwerty" },
+   ]);
+ //thêm mới
+   const addMember = (member) => {
+     setMembers([...members, member]);
+   };
+     // Hàm xóa thành viên theo ID
+     const removeMember = (id) => {
+       setMembers(members.filter(member => member.id !== id));
+   };
+ 
+   // Hàm cập nhật thông tin thành viên
+   const updateMember = (updatedMember) => {
+     setMembers(members.map(member => (member.id === updatedMember.id ? updatedMember : member)));
+   };
+ 
+ 
+   return (
+     <div className="container mt-4">
+       <h1 className="text-primary">Quản lý thành viên</h1>
+       <PvlMemberList members={members} removeMember={removeMember} updateMember={updateMember}/>
+       <PvlMemberAdd addMember={addMember}/>
+       
+     </div>
+   );
+ };
+ 
+ export default PvlApp;
